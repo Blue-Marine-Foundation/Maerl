e6933cca28c5b6aa357147e5770c7a77b16c6022
Sam Mead <samjamead@gmail.com>
2023-12-10 17:41:50 +0000

    Always-on dark mode

--- _components/SideNav/NavList.tsx
+++ _components/SideNav/NavList.tsx
@@ -19,7 +19,7 @@ export default function NavList({ data }: { data: NavItem[] }) {
           >
             <Link
               href={item.href}
-              className='inline-block w-[200px] mb-2 px-4 py-2 rounded-md border border-transparent transition-all ease-in-out duration-300 hover:border-foreground/20 group-[.active]:bg-gray-100 group-[.active]:border-gray-300 dark:group-[.active]:bg-slate-800 dark:group-[.active]:border-slate-800'
+              className='inline-block w-full mb-2 px-4 py-2 rounded-md border border-transparent transition-all ease-in-out duration-300 hover:border-foreground/20 group-[.active]:bg-[#2B3246]'
             >
               {item.name}
             </Link>
