38a95b15301ef2cf51eeaeb6c366dd402ba1b684
Sam Mead <samjamead@gmail.com>
2023-12-11 21:59:44 +0000

    Tweak account pages (refactor as server side)

--- app/app/account/page.tsx
+++ app/app/account/page.tsx
@@ -1,61 +1,49 @@
-'use client';
+import { createClient } from '@/utils/supabase/server';
 
-import { createClient } from '@/utils/supabase/client';
-import { useState, useEffect } from 'react';
+// interface User {
+//   display_name: string;
+//   first_name: string;
+//   last_name: string;
+//   role: string;
+// }
 
-interface User {
-  display_name: string;
-  first_name: string;
-  last_name: string;
-  role: string;
-}
-
-export default function Account() {
+export default async function Account() {
   const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  const [userProfile, setUserProfile] = useState<User[] | undefined>();
-  const [isError, setIsError] = useState(false);
-
-  async function fetchUsers() {
-    const { data: users, error } = await supabase.from('users').select('*');
-
-    if (error) {
-      // eslint-disable-next-line no-console
-      console.error('Error fetching users:', error);
-      setIsError(true);
-      return;
-    }
-
-    if (users) {
-      setIsError(false);
-      setUserProfile(users);
-    }
+  if (!user) {
+    return <p className='mb-8'>Welcome back!</p>;
   }
 
-  useEffect(() => {
-    fetchUsers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { data: userDetails } = await supabase
+    .from('users')
+    .select('*')
+    .eq('id', user.id);
 
   return (
-    <div className='w-full px-8 pt-8 pb-16 border rounded-md'>
-      {userProfile &&
-        userProfile.map(({ display_name, first_name, last_name, role }) => (
-          <div key={display_name} className='mb-4'>
-            <h2 className='text-2xl font-bold mb-8'>
-              {first_name} {last_name}
-            </h2>
-
-            <dl className='w-[400px] mb-2 flex justify-start items-center gap-2'>
-              <dt className='font-medium w-[100px]'>Username:</dt>
-              <dd>@{display_name}</dd>
-            </dl>
-            <dl className='w-[400px] flex justify-start items-center gap-2'>
-              <dt className='font-medium w-[100px]'>Role:</dt>
-              <dd>{role}</dd>
-            </dl>
-          </div>
-        ))}
+    <div className='bg-card-bg p-8 mb-16 rounded-md shadow'>
+      <div className='mb-4 flex justify-start items-start gap-4'>
+        <div className='w-[100px]'>
+          <p className='text-foreground/70'>Name</p>
+        </div>
+        <div>
+          <p>
+            {userDetails
+              ? `${userDetails[0].first_name} ${userDetails[0].last_name}`
+              : ''}
+          </p>
+        </div>
+      </div>
+      <div className='mb-4 flex justify-start items-start gap-4'>
+        <div className='w-[100px]'>
+          <p className='text-foreground/70'>Role</p>
+        </div>
+        <div>
+          <p>{userDetails ? `${userDetails[0].role}` : ''}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- app/app/account/passwordreset/page.tsx
+++ app/app/account/passwordreset/page.tsx
@@ -1,17 +1,16 @@
-import Link from 'next/link';
 import FormMessages from '@/components/FormMessages';
 
 export default function PasswordReset() {
   return (
     <>
-      <div className='w-full px-8 py-16 border rounded-md'>
+      <div className='w-full px-8 py-16 bg-card-bg rounded-md shadow mb-16'>
         <form
           className='max-w-sm mx-auto flex-1 flex flex-col w-full justify-center gap-2 text-foreground '
           action='/auth/passwordreset'
           method='post'
         >
           <label className='text-md' htmlFor='new_password'>
-            New password
+            Enter a new password:
           </label>
           <input
             className='rounded-md px-4 py-2 bg-inherit border mb-6'
@@ -25,7 +24,7 @@ export default function PasswordReset() {
 
           <FormMessages />
 
-          <button className='bg-green-700 text-white rounded-md px-4 py-2 text-foreground mb-2'>
+          <button className='bg-btn-background hover:bg-btn-background-hover text-white rounded-md px-4 py-2 text-foreground mb-2'>
             Request password reset
           </button>
         </form>
