9967e9935fdebba2914d9339c309c559a42d43e1
Sam Mead <samjamead@gmail.com>
2023-12-16 09:40:26 +0000

    Give long project names more space in page header

--- app/app/projects/[slug]/layout.tsx
+++ app/app/projects/[slug]/layout.tsx
@@ -13,39 +13,42 @@ export default async function ProjectLayout({
 }) {
   const supabase = createClient();
 
-  const { data: projects, error } = await supabase
+  const { data: project, error } = await supabase
     .from('projects')
     .select('*, outputs (*), updates (*)')
-    .eq('slug', params.slug);
+    .eq('slug', params.slug)
+    .single();
+
+  console.log(project);
 
   return (
     <>
-      <div className='flex justify-start items-center pt-4 pb-8 border-b border-foreground/10'>
-        {projects ? (
+      <div className='flex justify-between items-center pt-4 pb-8 border-b border-foreground/10'>
+        {project ? (
           <>
-            <div className='basis-1/5 flex justify-start items-center gap-6'>
+            <div className='basis-2/5 flex justify-start items-center gap-6'>
               <span
                 style={{
-                  background: projects[0].highlight_color,
+                  background: project.highlight_color,
                 }}
                 className='flex justify-center items-center w-10 h-10 text-lg font-bold text-background rounded-md'
               >
-                {projects[0].id}
+                {project.id}
               </span>
-              <h2 className='text-2xl font-bold'>{params.slug}</h2>
+              <h2 className='text-2xl font-bold'>{project.name}</h2>
             </div>
-            <div className='basis-4/5 pl-8 flex justify-start items-center gap-8 text-xs font-mono text-foreground/80'>
+            <div className='basis-3/5 pl-8 flex justify-end items-center gap-8 text-xs font-mono text-foreground/80'>
               <p className='px-2 py-1 rounded bg-green-300 text-card-bg'>
                 Last updated{' '}
-                {projects[0].updates.length > 0
-                  ? projects[0].updates.at(-1).created_at.slice(0, 10)
+                {project.updates.length > 0
+                  ? project.updates.at(-1).created_at.slice(0, 10)
                   : 'never'}
               </p>
               <p className='px-2 py-1 rounded bg-pink-300 text-card-bg'>
-                {projects[0].outputs.length} outputs
+                {project.outputs.length} outputs
               </p>
               <p className='px-2 py-1 rounded bg-purple-300 text-card-bg'>
-                {projects[0].updates.length} updates
+                {project.updates.length} updates
               </p>
             </div>
           </>
