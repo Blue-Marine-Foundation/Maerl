09f5747605f92e8528a91573d6ff14b64db5c5ad
Sam Mead <samjamead@gmail.com>
2023-12-23 16:09:03 +0000

    Tinkering w/ logframes

--- _components/LogframeFeatureCard.tsx
+++ _components/LogframeFeatureCard.tsx
@@ -21,43 +21,34 @@ export default function LogframeFeatureCard({
   assumption?: string;
 }) {
   return (
-    <div className='p-8 mb-8 bg-card-bg rounded-md'>
-      <div className='flex gap-4 mb-8 text-sm font-mono'>
-        <Link
-          href={`/app/projects/${project}/logframe/${
-            code?.startsWith('OC.') ? 'outcome' : 'output'
-          }?id=${id}&code=${code}`}
-          className='px-2 py-1 rounded bg-pink-300 text-card-bg'
-        >
+    <Link
+      href={`/app/projects/${project}/logframe/${
+        code?.startsWith('OC.') ? 'outcome' : 'output'
+      }?id=${id}&code=${code}`}
+      className='block pt-4 px-4 pb-5 max-w-4xl bg-card-bg hover:bg-card-bg/60 border-b first-of-type:rounded-t-lg first-of-type:pt-5 last-of-type:rounded-b-lg last-of-type:pb-5 last-of-type:border-b-transparent'
+    >
+      <div className='flex justify-start items-baseline gap-8 text-sm'>
+        <span className='px-2 py-1 text-sm font-mono rounded bg-pink-300 text-card-bg'>
           {code}
-        </Link>
-        {target && (
-          <p className='px-2 py-1 rounded bg-purple-300 text-card-bg'>
-            {target}
-          </p>
-        )}
+        </span>
+
+        <div className='flex flex-col gap-4 max-w-2xl'>
+          {children}
+          {/* {verification && (
+            <p className='text-foreground/75'>Verified by: {verification}</p>
+          )}
+          {assumption && (
+            <div className='flex gap-4 text-sm'>
+              <div className='basis-1/5'>
+                <p className='text-foreground/80'>Assumption</p>
+              </div>
+              <div className='basis-4/5'>
+                <p>{assumption}</p>
+              </div>
+            </div>
+          )} */}
+        </div>
       </div>
-      {children}
-      {verification && (
-        <div className='flex gap-4 mb-4 text-sm'>
-          <div className='basis-1/5'>
-            <p className='text-foreground/80'>Verified by</p>
-          </div>
-          <div className='basis-4/5'>
-            <p>{verification}</p>
-          </div>
-        </div>
-      )}
-      {assumption && (
-        <div className='flex gap-4 text-sm'>
-          <div className='basis-1/5'>
-            <p className='text-foreground/80'>Assumption</p>
-          </div>
-          <div className='basis-4/5'>
-            <p>{assumption}</p>
-          </div>
-        </div>
-      )}
-    </div>
+    </Link>
   );
 }
