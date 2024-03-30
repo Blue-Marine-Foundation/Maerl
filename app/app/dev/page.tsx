c3a073a1fc9df8bc300c223f030da3919bdbf08a
Sam Mead <samjamead@gmail.com>
2024-03-30 19:04:41 +0000

    Add dev page for testing

---
+++ app/app/dev/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { createClient } from '@/_utils/supabase/client';
+import { useState, useEffect } from 'react';
+
+export default function Page() {
+  const supabase = createClient();
+
+  const [data, setData] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    const { data, error } = await supabase
+      .from('updates')
+      .select('*')
+      .is('output_measurable_id', null);
+
+    if (error) {
+      console.log(error);
+    }
+
+    if (data) {
+      console.log(data);
+      setData(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <p>The number of posts: {data && data.length}</p>;
+}
