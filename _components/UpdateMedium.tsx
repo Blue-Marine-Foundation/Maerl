eeb42eb72c3f073ae0ed715eeb7a13a1b1a33019
Sam Mead <samjamead@gmail.com>
2024-03-30 21:45:44 +0000

    Tinkering with updates

--- _components/UpdateMedium.tsx
+++ _components/UpdateMedium.tsx
@@ -6,48 +6,60 @@ import dayjs from 'dayjs';
 export default function UpdateMedium({ update }: { update: Update }) {
   return (
     <div className='text-white text-sm p-4 flex justify-between items-baseline gap-4 bg-card-bg border-b first-of-type:pt-5 first-of-type:rounded-t-lg last-of-type:border-b-transparent last-of-type:rounded-b-lg shadow'>
-      <div className='w-[180px]'>
+      <div className='w-[190px]'>
         <Link
-          style={{ background: update.projects.highlight_color }}
           href={`/app/projects/${update.projects.slug}`}
-          className='py-1 px-2 text-xs text-background rounded-md'
+          className='flex items-center gap-4 mb-1'
         >
+          <span
+            className='h-2 w-2 rounded-full'
+            style={{ background: update.projects.highlight_color }}
+          ></span>
           {update.projects.name}
         </Link>
-      </div>
-      <div className='grow'>
-        <p className='text-sm max-w-md'>{update.description}</p>
-        <div className='flex gap-2'></div>
-      </div>
-      <div className='flex gap-4 text-xs font-mono'>
-        <p className='text-foreground/70'>
-          {dayjs(update.date).format('DD MMM')}
+        <p className='pl-6 text-xs text-foreground/70'>
+          {dayjs(update.date).format('DD MMM YY')}
         </p>
+      </div>
+      <div className='w-[200px]'>
         {update.output_measurable_id && (
-          <>
-            /
+          <p className='mb-1'>
             <Tooltip
               tooltipContent={update.output_measurables?.description}
               tooltipWidth={380}
-              tooltipDirection='right'
+              tooltipDirection='left'
             >
-              {update.output_measurables.code}
+              Output {update.output_measurables.code}
             </Tooltip>{' '}
-          </>
+          </p>
         )}
-        /
+
         {update.output_measurables?.impact_indicators?.indicator_code ? (
-          <Tooltip
-            tooltipContent={
-              update.output_measurables.impact_indicators.indicator_title
-            }
-            tooltipWidth={380}
-            tooltipDirection='right'
-          >
-            {update.output_measurables.impact_indicators.indicator_code}
-          </Tooltip>
+          <p className='text-xs text-foreground/70'>
+            <Tooltip
+              tooltipContent={
+                update.output_measurables.impact_indicators.indicator_title
+              }
+              tooltipWidth={380}
+              tooltipDirection='left'
+            >
+              Impact indicator{' '}
+              {update.output_measurables.impact_indicators.indicator_code}
+            </Tooltip>
+          </p>
         ) : (
-          <span>Progress</span>
+          <p>Progress update</p>
+        )}
+      </div>
+      <div className='grow'>
+        <p className='max-w-lg'>{update.description}</p>
+      </div>
+      <div className='w-[200px]'>
+        <p className=''>{update.value}</p>
+        {update.output_measurables?.impact_indicators && (
+          <p className='text-xs text-foreground/70'>
+            {update.output_measurables.impact_indicators.indicator_unit}
+          </p>
         )}
       </div>
     </div>
