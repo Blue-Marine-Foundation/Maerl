ecd1427565b9a30a8ca1eb08e2e77e6f495c1b78
Sam Mead <samjamead@gmail.com>
2024-03-30 19:05:52 +0000

    Nudge footer padding

--- _components/footer/footer.tsx
+++ _components/footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className='px-4 w-full border-t border-t-foreground/10 pt-12 pb-28 '>
-      <div className='max-w-7xl mx-auto flex justify-between'>
+    <footer className='w-full border-t border-t-foreground/10 pt-12 pb-28 '>
+      <div className='max-w-7xl px-4 mx-auto flex justify-between'>
         <p className='mb-[2em] text-sm'>Powered by Blue Marine Foundation</p>
         <p className='text-xs'>
           &copy; Blue Marine Foundation, 2023 and beyond...
