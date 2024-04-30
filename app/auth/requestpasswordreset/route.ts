381d0c32d6c40ff7282e789e717152821749a5e8
Sam Mead <samjamead@gmail.com>
2024-04-30 18:36:15 +0100

    Improve error handling for password reset requests

--- app/auth/requestpasswordreset/route.ts
+++ app/auth/requestpasswordreset/route.ts
@@ -16,7 +16,7 @@ export async function POST(request: Request) {
   if (error) {
     console.log(error);
     return NextResponse.redirect(
-      `${PRODUCTION_URL}/requestpasswordreset?error=Could not send reset password email`,
+      `${PRODUCTION_URL}/requestpasswordreset?error=${error.message}`,
       {
         // a 301 status is required to redirect from a POST to a GET route
         status: 301,
