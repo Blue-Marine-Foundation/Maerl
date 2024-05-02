5fb8d930f632c3bedb37c3f2d551b6825336105c
Sam Mead <samjamead@gmail.com>
2024-05-02 20:48:09 +0100

    Update login route handling

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,16 +1,13 @@
-import { PRODUCTION_URL } from '@/lib/constants';
 import { createClient } from '@/utils/supabase/server';
 import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
-
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the Auth Helpers package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
+  // by the SSR package. It exchanges an auth code for the user's session.
+  // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-  const next = requestUrl.searchParams.get('next');
+  const origin = requestUrl.origin;
 
   if (code) {
     const supabase = createClient();
@@ -18,10 +15,5 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-  return next
-    ? // If next is specifed go to that route
-      NextResponse.redirect(`${PRODUCTION_URL}/${next}`)
-    : // Otherwise go to dashboard home.
-      // Will reroute to homepage if no user found
-      NextResponse.redirect(`${PRODUCTION_URL}/app`);
+  return NextResponse.redirect(`${origin}/app`);
 }
