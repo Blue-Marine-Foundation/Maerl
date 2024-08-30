54eb4942702eebdd3674592650cdbcf2901e5779
Sam Mead <samjamead@gmail.com>
2024-08-30 15:26:13 +0100

    Update updates plot from month to week

--- _components/homepage-update-plot/homepage-update-plot.tsx
+++ _components/homepage-update-plot/homepage-update-plot.tsx
@@ -1,33 +1,56 @@
-'use client';
+'use client'
 
-import * as Plot from "@observablehq/plot";
-import ClientPlotFigure from '../PlotFigureClient';
-import { Update } from "@/_lib/types";
+import * as Plot from '@observablehq/plot'
+import ClientPlotFigure from '../PlotFigureClient'
+import { Update } from '@/_lib/types'
 
-export default async function HomepageUpdatePlot({updates}: {updates: Update[]} ) {
+export default async function HomepageUpdatePlot({
+  updates,
+}: {
+  updates: Update[]
+}) {
   return (
     <ClientPlotFigure
       options={{
+        marginTop: 40,
         x: {
           grid: true,
+          label: 'Updates per week',
+          labelAnchor: 'center',
+          labelOffset: 35,
+          axis: 'top',
         },
         y: {
           line: true,
         },
         marks: [
-          Plot.areaX(updates, Plot.binY({x: "count"}, {
-            y: d => new Date(d.date),
-            interval: 'month',
-            fill: 'skyblue',
-            fillOpacity: 0.5,
-            curve: 'catmull-rom'
-          })),
-          Plot.lineX(updates, Plot.binY({x: "count"}, {
-            y: d => new Date(d.date),
-            interval: 'month',
-            curve: 'catmull-rom'
-          })),
-        ]
+          Plot.areaX(
+            updates,
+            Plot.binY(
+              { x: 'count' },
+              {
+                y: (d) => new Date(d.date),
+                interval: 'week',
+                // @ts-expect-error
+                fill: 'skyblue',
+                fillOpacity: 0.5,
+                curve: 'catmull-rom',
+              }
+            )
+          ),
+          Plot.lineX(
+            updates,
+            Plot.binY(
+              { x: 'count' },
+              {
+                y: (d) => new Date(d.date),
+                interval: 'week',
+                // @ts-expect-error
+                curve: 'catmull-rom',
+              }
+            )
+          ),
+        ],
       }}
     />
   )
