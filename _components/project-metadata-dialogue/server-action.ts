0df92f09735264f0b641ad91e8ea2909e2375e91
Sam Mead <samjamead@gmail.com>
2024-09-26 22:37:00 +0100

    Update server action to remove old metadata keys

--- _components/project-metadata-dialogue/server-action.ts
+++ _components/project-metadata-dialogue/server-action.ts
@@ -1,19 +1,19 @@
-'use server';
+'use server'
 
-import { ProjectMetadata } from '@/_lib/types';
-import { createClient } from '@/_utils/supabase/server';
+import { ProjectMetadata } from '@/_lib/types'
+import { createClient } from '@/_utils/supabase/server'
 
 export default async function ProjectMetadataServerAction(
   values: ProjectMetadata
 ) {
-  const { users, impacts, outcomes, ...update } = values;
+  const { users, ...update } = values
 
-  const supabase = createClient();
+  const supabase = createClient()
 
   const result = await supabase
     .from('projects')
     .upsert(update, { onConflict: 'id' })
-    .select();
+    .select()
 
-  return result;
+  return result
 }
