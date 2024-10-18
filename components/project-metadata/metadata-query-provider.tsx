2779843df5a1c95a10f904a06d3ed23a348ba4c9
Sam Mead <samjamead@gmail.com>
2024-10-18 15:05:20 +0100

    Initialise queryClient as state

--- components/project-metadata/metadata-query-provider.tsx
+++ components/project-metadata/metadata-query-provider.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function MetadataQueryProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
