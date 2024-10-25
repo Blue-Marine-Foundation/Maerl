5aedc3ba6e0aafd2144e5ca1d22efbf92c7b581f
Sam Mead <samjamead@gmail.com>
2024-10-25 11:35:23 +0100

    Make query provider available to project subpages

--- app/(protected)/projects/[slug]/page.tsx
+++ app/(protected)/projects/[slug]/page.tsx
@@ -28,24 +28,25 @@ export default async function ProjectPage({
   const project = { ...data, pm };
 
   return (
-    <div className='grid grid-cols-3 items-start gap-4'>
-      <QueryProvider>
+    <QueryProvider>
+      <div className='grid grid-cols-3 items-start gap-4'>
         <ProjectMetadataDisplay project={project} />
-      </QueryProvider>
-      <LogframeCard projectId={project.id} slug={slug} />
-      <div className='flex min-h-64 flex-col rounded-md bg-card p-4'>
-        <h3 className='mb-auto text-sm font-semibold text-muted-foreground'>
-          Funding requests
-        </h3>
-        <div className='flex flex-grow flex-col items-center justify-center gap-2'>
-          <p className='text-foreground/80'>
-            This project has no funding requests yet
-          </p>
-          <button className='mt-2 flex items-center gap-2 rounded-md border border-dashed px-3 py-1.5 text-sm text-foreground/80 transition-all hover:border-solid hover:border-foreground/50 hover:text-foreground'>
-            <PlusCircleIcon className='h-4 w-4' /> Create funding request
-          </button>
+
+        <LogframeCard projectId={project.id} slug={slug} />
+        <div className='flex min-h-64 flex-col rounded-md bg-card p-4'>
+          <h3 className='mb-auto text-sm font-semibold text-muted-foreground'>
+            Funding requests
+          </h3>
+          <div className='flex flex-grow flex-col items-center justify-center gap-2'>
+            <p className='text-foreground/80'>
+              This project has no funding requests yet
+            </p>
+            <button className='mt-2 flex items-center gap-2 rounded-md border border-dashed px-3 py-1.5 text-sm text-foreground/80 transition-all hover:border-solid hover:border-foreground/50 hover:text-foreground'>
+              <PlusCircleIcon className='h-4 w-4' /> Create funding request
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </QueryProvider>
   );
 }
