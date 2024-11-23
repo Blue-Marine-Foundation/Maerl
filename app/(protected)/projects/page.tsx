dace77bdeae0d8d5ad8e4423c9b44ab76888181e
Sam Mead <samjamead@gmail.com>
2024-11-23 16:23:25 +0000

    Move query provider to root layout

--- app/(protected)/projects/page.tsx
+++ app/(protected)/projects/page.tsx
@@ -1,14 +1,11 @@
-import QueryProvider from '@/utils/query-provider';
 import ProjectsDataTableWrapper from '@/components/projects-data-table/data-table-wrapper';
 import PageHeading from '@/components/ui/page-heading';
 
 export default function ProtectedPage() {
   return (
-    <QueryProvider>
-      <div className='flex w-full flex-col gap-8'>
-        <PageHeading>Projects</PageHeading>
-        <ProjectsDataTableWrapper />
-      </div>
-    </QueryProvider>
+    <div className='flex w-full flex-col gap-8'>
+      <PageHeading>Projects</PageHeading>
+      <ProjectsDataTableWrapper />
+    </div>
   );
 }
