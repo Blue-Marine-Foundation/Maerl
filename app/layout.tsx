37d55c82bd56476ab588fc7c0906ccb9b1a320ac
Sam Mead <samjamead@gmail.com>
2024-11-26 18:40:05 +0000

    Put the updates in the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/header/alt-header';
+import Header from '@/components/header/header';
 import Footer from '@/components/footer/footer';
 import QueryProvider from '@/utils/query-provider';
 
