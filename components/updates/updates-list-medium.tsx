bc03d701b8f12072ff5325c83d688eebf592d346
Sam Mead <samjamead@gmail.com>
2024-12-13 11:47:37 +0000

    Update updates-list-medium.tsx
    Closes #132

--- components/updates/updates-list-medium.tsx
+++ components/updates/updates-list-medium.tsx
@@ -7,18 +7,18 @@ export default function UpdatesListMedium({ updates }: { updates: Update[] }) {
     <div className='flex flex-col'>
       {updates?.map((update) => (
         <div
-          className='grid grid-cols-[165px_160px_1fr] items-baseline gap-12 border-t py-6 first:border-t-0 first:pt-0 hover:bg-muted/50'
+          className='grid grid-cols-[140px_160px_1fr] items-baseline gap-6 border-t py-6 first:border-t-0 first:pt-0'
           key={update.id}
         >
           <div className='flex flex-col gap-2'>
             <Link
-              className='max-w-48 truncate text-sm font-medium hover:underline'
+              className='max-w-[124px] truncate text-sm font-medium hover:underline'
               href={`/projects/${update.projects?.slug}`}
             >
               {update.projects?.name}
             </Link>
             <span className='text-xs text-muted-foreground'>
-              {d3.timeFormat('%d %b %Y')(new Date(update.created_at))}
+              {d3.timeFormat('%d %b %Y')(new Date(update.date))}
             </span>
           </div>
           <div className='flex flex-col items-start justify-start gap-2'>
@@ -30,6 +30,7 @@ export default function UpdatesListMedium({ updates }: { updates: Update[] }) {
                 {update.output_measurables.output_id ? (
                   <Link
                     href={`/projects/${update.projects?.slug}/logframe/output?id=${update.output_measurables.output_id}`}
+                    className='hover:underline'
                   >
                     Output {update.output_measurables.code}
                   </Link>
@@ -48,16 +49,22 @@ export default function UpdatesListMedium({ updates }: { updates: Update[] }) {
             )}
           </div>
           <div className='flex flex-col gap-2'>
-            <p className='text-sm'>{update.description}</p>
             {update.impact_indicators && update.value && (
-              <p className='text-sm text-muted-foreground'>
-                {update.impact_indicators.indicator_unit}:{' '}
-                {d3.format(',')(update.value)}
+              <p className='text-sm'>
+                {d3.format(',')(update.value)}{' '}
+                <span className='text-muted-foreground'>
+                  {update.impact_indicators.indicator_unit}
+                </span>
               </p>
             )}
             {update.type === 'Progress' && (
               <p className='text-sm text-muted-foreground'>Progress update</p>
             )}
+            <p className='text-sm'>{update.description}</p>
+            {/* <p className='text-xs text-muted-foreground'>
+              Added to Maerl{' '}
+              {d3.timeFormat('%d %b %Y')(new Date(update.created_at))}
+            </p> */}
           </div>
         </div>
       ))}
