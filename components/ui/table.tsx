7a4d8af06a271c5b6758f058f3115f0e363e3aad
Sam Mead <samjamead@gmail.com>
2025-06-15 13:59:08 +0100

    Improve admin table sticky header
    Closes #191

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -6,10 +6,10 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className='relative w-full'>
+  <div className='relative'>
     <table
       ref={ref}
-      className={cn('w-full caption-bottom text-sm', className)}
+      className={cn('caption-bottom text-sm', className)}
       {...props}
     />
   </div>
